a1f31ee8e71f5fa9b2e70183a43b09fddd7f1470
ETT7 <66707460+ETT7@users.noreply.github.com>
2022-02-25 21:18:40 +0900

    Update treeview.py

--- treeview.py
+++ treeview.py
@@ -1,7 +1,7 @@
 import os
 import logging
 
-path = os.path.join(os.getcwd(),'scan_dir')
+path = os.path.join(os.getcwd(),'scan\\')
 logging.basicConfig(filename="scaned_dirs_treeview.log", 
 					format='%(message)s', 
 					filemode='a+')
@@ -11,6 +11,8 @@ for dirpath, dirnames, filenames in os.walk(path):
     directory_level = dirpath.replace(path, "")
     directory_level = directory_level.count(os.sep)
     indent = " " * 4
-    print("{}{}/".format(indent*directory_level, os.path.basename(dirpath)))
+    print("{}{}\\".format(indent*directory_level, os.path.basename(dirpath)))
+    logger.info("{}{}\\".format(indent*directory_level, os.path.basename(dirpath)))
     for f in filenames:
+        print("{}{}".format(indent*(directory_level+1), f))
         logger.info("{}{}".format(indent*(directory_level+1), f))
